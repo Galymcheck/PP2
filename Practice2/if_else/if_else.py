6aece7837cb5b668221b55ebd7452ff938856edb
Galamat <galamattolepkazy31843184@gmail.com>
2026-02-05 12:19:15 +0500

    Update if_else examples

--- Practice2/if_else/if_else.py
+++ Practice2/if_else/if_else.py
@@ -1,8 +1,6 @@
 a = 200
 b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
+if b == a:
   print("a and b are equal")
 else:
-  print("a is greater than b")
+  print("a and b are not equal")
