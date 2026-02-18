d83ebb749c8a3fe58b274b050d0ecc1b8fbec8ed
Galamat <galamattolepkazy31843184@gmail.com>
2026-02-18 11:43:35 +0500

    Python args_kwargs examples  add

---
+++ functions/args_kwargs.py
@@ -0,0 +1,25 @@
+def my_function(*args): #allows a function to accept any number of positional arguments.
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
+
+def add_numbers(*args):
+    total = 0
+    for n in args:
+        total += n
+    return total
+
+print(add_numbers(1, 2, 3))
+print(add_numbers(5, 10, 15, 20))
+
+
+def my_function(**kwargs):
+  print("Type:", type(kwargs))
+  print("Name:", kwargs["name"])
+  print("Age:", kwargs["age"])
+  print("All data:", kwargs)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
