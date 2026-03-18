fa6ac4a57848abef36278bf67ff7b8cd928d4846
Galamat <galamattolepkazy31843184@gmail.com>
2026-03-18 08:44:47 +0500

    Add create_list examples

---
+++ Practice6/directory_management/create_list_dirs.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+os.makedirs("project/data/files", exist_ok=True)
+file1 = open("project/data/files/123.txt", "w")
+file1.write("Hello")
+file1.close()
+shutil.copy("project/data/files/123.txt", "project/123-copy.txt")
+print(os.listdir("project"))
+
+for file in os.listdir("project"):
+    if file.endswith(".txt"):
+        print("txt file:", file)
+shutil.rmtree("project")
